de8520162292a9101f9fbfc894cbfcb86eefa300
Thongangerge <126161416+Thongangerge@users.noreply.github.com>
2024-04-18 15:35:00 +0900

    Create setup.py
    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+  name = 'ppt-to-pdf-converter',
+  version='0.0.1',
+  description='convert ppt files to pdf in folder',
+  author = 'jhheo',
+  url = 'https://github.com/Thongangerge/ppt-to-pdf-converter',
+  download_url= 'https://github.com/Thongangerge/ppt-to-pdf-converter',
+  packages= [ 'code' ],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+  ]
+)
